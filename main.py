3aa8f144c1b3ff802382d530756468278a4cba96
mmyciel-macwro <mmycielski@pgs-soft.com>
2022-09-14 18:44:18 +0200

    model changed

--- main.py
+++ main.py
@@ -28,11 +28,9 @@ def get_batch(batch_size=64):
     np.random.shuffle(batch_y)
 
     class_list = np.random.randint(start, end, batch_size)
-    batch_x.append(np.zeros((batch_size, 2)))
-    batch_x.append(np.zeros((batch_size, 2)))
-    print(temp_x.shape)
-    print(temp_cat_list)
-    print(class_list[0])
+    batch_x.append(np.zeros((batch_size, 10, 2)))
+    batch_x.append(np.zeros((batch_size, 10, 2)))
+
     for i in range(0, batch_size):
         batch_x[0][i] = temp_x[np.random.choice(class_list)]
         # If train_y has 0 pick from the same class, else pick from any other class
@@ -53,11 +51,11 @@ def nway_one_shot(model, n_way, n_val):
     x_0_choice = []
     n_correct = 0
 
-    class_list = np.random.randint(train_size + 1, len(x) - 1, n_val)
+    class_list = np.random.randint(train_size + 1, len(cat_list) - 1, n_val)
 
     for i in class_list:
         j = np.random.choice(cat_list[i])
-        temp = [np.zeros((n_way, 100, 100, 3)), np.zeros((n_way, 100, 100, 3))]
+        temp = [np.zeros((n_way, 10, 2)), np.zeros((n_way, 10, 2))]
         for k in range(0, n_way):
             temp[0][k] = x[j]
 
@@ -81,7 +79,7 @@ def nway_one_shot(model, n_way, n_val):
 light_dir = r'dataset/light_preprocessed'
 sound_dir = r'dataset/sound_preprocessed'
 train_test_split = 0.7
-no_of_items_in_each_class = 100
+no_of_items_in_each_class = 300
 
 # Read all the folders in the directory
 
@@ -92,18 +90,24 @@ x = []
 y = []
 y_label = 0
 temp = []
-# Using just 5 images per category
 for file_name in os.listdir(light_dir):
     temp = []
+    temp_x = []
+    counter = 0
     with open(os.path.join(light_dir, file_name)) as light_file:
         with open(os.path.join(sound_dir, file_name)) as sound_file:
             sound_reader = csv.reader(sound_file)
             light_reader = csv.reader(light_file)
             for row in light_reader:
-                temp.append(len(x))
                 sound = sound_reader.__next__()[0]
-                x.append(np.asarray([row[0], sound]))
-                y.append(y_label)
+                temp_x.append(np.asarray([row[0], sound]))
+                counter += 1
+                if counter == 10:
+                    temp.append(len(x))
+                    x.append(temp_x)
+                    y.append(y_label)
+                    counter = 0
+                    temp_x = []
     y_label += 1
     cat_list.append(temp)
 
@@ -111,24 +115,25 @@ cat_list = np.asarray(cat_list)
 x = np.asarray(x)
 y = np.asarray(y)
 print('X, Y shape', x.shape, y.shape, cat_list.shape)
-train_size = int(len(x) * train_test_split)
-test_size = len(x) - train_size
+train_size = int(len(cat_list) * train_test_split)
+test_size = len(cat_list) - train_size
 print(train_size, 'classes for training and', test_size, ' classes for testing')
 
 # Training Split
-x_train = x[:train_size]
-y_train = y[:train_size]
-cat_train = cat_list[:int(train_size / 100)]
+train_files = train_size * no_of_items_in_each_class
+x_train = x[:train_files]
+y_train = y[:train_files]
+cat_train = cat_list[:train_size]
 
 # Validation Split
-x_val = x[train_size:]
-y_val = y[train_size:]
-cat_test = cat_list[int(train_size / 100):]
+x_val = x[train_files:]
+y_val = y[train_files:]
+cat_test = cat_list[train_size:]
 
 print('X&Y shape of training data :', x_train.shape, 'and', y_train.shape, cat_train.shape)
 print('X&Y shape of testing data :', x_val.shape, 'and', y_val.shape, cat_test.shape)
 
-input_shape = (2,)
+input_shape = (10, 2)
 left_input = Input(input_shape)
 right_input = Input(input_shape)
 
@@ -136,18 +141,8 @@ W_init = keras.initializers.RandomNormal(mean=0.0, stddev=1e-2)
 b_init = keras.initializers.RandomNormal(mean=0.5, stddev=1e-2)
 
 model = keras.models.Sequential([
-    keras.layers.Conv2D(64, (10, 10), activation='relu', input_shape=input_shape, kernel_initializer=W_init,
-                        bias_initializer=b_init, kernel_regularizer=l2(2e-4)),
-    keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(128, (7, 7), activation='relu', kernel_initializer=W_init, bias_initializer=b_init,
-                        kernel_regularizer=l2(2e-4)),
-    keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(128, (4, 4), activation='relu', kernel_initializer=W_init, bias_initializer=b_init,
-                        kernel_regularizer=l2(2e-4)),
-    keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(256, (4, 4), activation='relu', kernel_initializer=W_init, bias_initializer=b_init,
-                        kernel_regularizer=l2(2e-4)),
-    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(64, activation='relu', kernel_initializer=W_init, bias_initializer=b_init),
     keras.layers.Flatten(),
     keras.layers.Dense(4096, activation='sigmoid', kernel_initializer=W_init, bias_initializer=b_init)
 ])
@@ -164,10 +159,10 @@ siamese_net.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 plot_model(siamese_net, show_shapes=True, show_layer_names=True)
 
-epochs = 10
+epochs = 1001
 n_way = 20
 n_val = 100
-batch_size = 64
+batch_size = 256
 
 loss_list = []
 accuracy_list = []
@@ -176,7 +171,7 @@ for epoch in range(1, epochs):
     loss = siamese_net.train_on_batch(batch_x, batch_y)
     loss_list.append((epoch, loss))
     print('Epoch:', epoch, ', Loss:', loss)
-    if epoch % 250 == 0:
+    if epoch % 25 == 0:
         print("=============================================")
         accuracy = nway_one_shot(model, n_way, n_val)
         accuracy_list.append((epoch, accuracy))
--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
 import csv
 import os
+from numpy import mean
+from numpy import std
 
 
 def preprocess_data(input_path, output_path):
@@ -9,9 +11,11 @@ def preprocess_data(input_path, output_path):
             data = []
             for row in csv_reader:
                 data.append(float(row[0]))
-
-        data = sorted(data)
+        data_mean, data_std = mean(data), std(data)
+        cut_off = data_std * 3
+        data_without_outliers = [x for x in data if x >= data_mean - cut_off and x <= data_mean + cut_off]
         with open(os.path.join(output_path, file_name), 'w') as output:
             csv_writer = csv.writer(output)
-            for item in data[(len(data) // 2 - 50):(len(data) // 2 + 50)]:
+            # for item in data[(len(data) // 2 - 1000):(len(data) // 2 + 1000)]:
+            for item in data_without_outliers[:3000]:
                 csv_writer.writerow([item])
